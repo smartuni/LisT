8e81bffd72a487bafa957f9a45be554cb4b59f01
Lydia <lydia.pflug@web.de>
2017-10-16 20:57:38 +0200

    implemented Sensor andSensorType (Backend)

--- LisT_Backend/src/main/java/de/haw/list/sensorcomponent/model/Sensor.java
+++ LisT_Backend/src/main/java/de/haw/list/sensorcomponent/model/Sensor.java
@@ -3,12 +3,27 @@
  */
 package de.haw.list.sensorcomponent.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import de.haw.list.sensorcomponent.util.SensorType;
+
 /**
  * Modelliert Sensor.
  * 
- * @author Lydia Pflug
- * 09.10.2017
+ * @author Lydia Pflug 09.10.2017
  */
+@Entity
 public class Sensor {
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private SensorType sensorType;
+	
+	private String Name;
+	
 
 }
---
+++ LisT_Backend/src/main/java/de/haw/list/sensorcomponent/util/SensorType.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package de.haw.list.sensorcomponent.util;
+
+/**
+ * Typen fuer Sensoren
+ * 
+ * @author Lydia Pflug
+ * 16.10.2017
+ */
+public enum SensorType {
+	
+	TEMPERATURE, LIGHT, PH;
+
+}
