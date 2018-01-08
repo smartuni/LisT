5ebdd5eee74c6a05463929dbc514680e5a8a0ad6
Lydia <lydia.pflug@web.de>
2018-01-08 10:25:56 +0100

    change angular code

--- LisT_Backend/src/main/webapp/src/app/app.module.ts
+++ LisT_Backend/src/main/webapp/src/app/app.module.ts
@@ -1,44 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-// import { FlexLayoutModule } from '@angular/flex-layout';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { SensorService } from './sensor.service';
-import { ValueService } from './value.service';
-import { StatisticsComponent } from './statistics/statistics.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { HomeComponent } from './home/home.component';
-import { OptionsComponent } from './options/options.component';
-
 
 @NgModule({
-    imports: [
-    BrowserModule,
-    HttpClientModule,
-    NgxChartsModule,
-    AppRoutingModule,
-    // FlexLayoutModule
-  ],
   declarations: [
-    AppComponent,
-    StatisticsComponent,
-    ToolbarComponent,
-    HomeComponent,
-    OptionsComponent
+    AppComponent
   ],
-
-  providers: [
-    SensorService,
-    ValueService
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-
