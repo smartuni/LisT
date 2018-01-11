309891c846ef37ae7772872f21e2631b8e636350
Unknown <hendrik.seemann@haw-hamburg.de>
2018-01-11 18:27:35 +0100

    added httpclient provider

--- LisT_Backend/src/main/webapp/src/app/app.module.ts
+++ LisT_Backend/src/main/webapp/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SensorService } from './sensor.service';
 import { ValueService } from './value.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,10 +14,10 @@ import { ValueService } from './value.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [
-      SensorService,
+    SensorService,
     ValueService],
   bootstrap: [AppComponent]
 })
