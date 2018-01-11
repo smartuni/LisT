ff5124630c919e0fd850b467916b6d034436820f
Unknown <hendrik.seemann@haw-hamburg.de>
2018-01-11 18:04:33 +0100

    fixed Tables

--- LisT_Backend/src/main/webapp/src/app/app.component.ts
+++ LisT_Backend/src/main/webapp/src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Injectable, Component } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
   })
 export class AppComponent {
+  allSensors: Response;
+  value: Response;
   title = 'Smartes Aquarium';
   result = '';
 
@@ -16,24 +20,25 @@ export class AppComponent {
 
   private printValuesSensor1(): void {
     this.result = 'loading...';
-    this.http.get(`/api/sensors/1/values/`).subscribe(response => this.result = response.text());
+    this.http.get(`/api/sensors/1/values/`).subscribe(value => this.value = value);
   }
   private printValuesSensor2(): void {
     this.result = 'loading...';
-    this.http.get(`/api/sensors/2/values/`).subscribe(response => this.result = response.text());
+    this.http.get(`/api/sensors/2/values/`).subscribe(value => this.value = value);
   }
     private printValuesSensor3(): void {
     this.result = 'loading...';
-    this.http.get(`/api/sensors/3/values/`).subscribe(response => this.result = response.text());
+    this.http.get(`/api/sensors/3/values/`).subscribe(value => this.value = value);
   }
     private printValuesSensor4(): void {
     this.result = 'loading...';
-    this.http.get(`/api/sensors/4/values/`).subscribe(response => this.result = response.text());
+    this.http.get(`/api/sensors/4/values/`).subscribe(value => this.value = value);
   }
 
   private printSensor(): void {
     this.result = 'loading...';
-    this.http.get(`/api/sensors`).subscribe(response => this.result = response.text());
+    // this.http.get(`/api/sensors`).subscribe(response => this.result = response.text()); response => this.result = response.text()
+    this.http.get('api/sensors').subscribe(sensors => this.allSensors = sensors);
   }
 
 
