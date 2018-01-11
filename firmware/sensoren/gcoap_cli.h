bef3d77e2974ad9f0ff4c02fa8c129a0fc777fd6
Unknown <katrin.moritz@haw-hamburg.de>
2018-01-11 19:11:31 +0100

    add pH treiber
    ES FUNKTIONIERT!

--- firmware/sensoren/gcoap_cli.h
+++ firmware/sensoren/gcoap_cli.h
@@ -5,11 +5,12 @@
 extern uint16_t req_count;
 extern phydat_t temp;
 extern phydat_t light;
+extern char ph[6];
 
 extern char msg_temp[10];
 extern char msg_light[10];
 
-#define PRINT_ON_CONSOLE 1
+#define PRINT_ON_CONSOLE 0
 
 //static 
 //size_t _send(uint8_t *buf, size_t len, char *addr_str, char *port_str);
