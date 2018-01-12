697c19ecd93b8089899a1d5bc73dea1cc622a3ba
Unknown <hendrik.seemann@haw-hamburg.de>
2018-01-12 16:44:28 +0100

    routing fix

--- LisT_Backend/src/main/webapp/src/app/app-routing.module.ts
+++ LisT_Backend/src/main/webapp/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { OptionsComponent } from './options/options.component';
 
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: AppComponent},
     {path: 'options', component: OptionsComponent},
 ];
@@ -14,7 +13,7 @@ export const routes: Routes = [
 @NgModule({
   imports: [ RouterModule.forRoot(routes)],
   exports: [ RouterModule ],
-  declarations: []
+
 })
 export class AppRoutingModule { }
 
